62f696f1912284b72be0b286909c0c72c7b313dd
Эмилия <152471759+MilkyWayXDYT@users.noreply.github.com>
2026-03-17 07:17:50 +0300

    .

--- VariableJourney/Assets/Scripts/TypeChange/TheHarpoon.cs
+++ VariableJourney/Assets/Scripts/TypeChange/TheHarpoon.cs
@@ -49,29 +49,33 @@ public class TheHarpoon : MonoBehaviour
         {
             Platform platform = joinObj.GetComponent<Platform>();
             float harpoonForce = platform.harpoonForce;
+
+            if (Vector3.Distance(player.position, joinObj.position) < platform.destroyDistance)
+            {
+                player.GetComponent<HarpoonSpawn>().sent = false;
+                Destroy(gameObject);
+            }
+
             if (joinObj.tag == "ToBeAttracted")
             {
-                joinObj.position = Vector3.MoveTowards(joinObj.position, player.position, harpoonForce * Time.deltaTime / joinObj.GetComponent<Rigidbody>().mass);
+                Vector3 newPos = new Vector3(player.position.x, joinObj.position.y, player.position.z);
+                joinObj.position = Vector3.MoveTowards(joinObj.position, newPos, harpoonForce * Time.deltaTime / joinObj.GetComponent<Rigidbody>().mass);
             }
             else if (joinObj.tag == "Attracted")
             {
-                player.position = Vector3.MoveTowards(player.position, joinObj.position, harpoonForce * Time.deltaTime / player.GetComponent<Rigidbody>().mass);
                 RaycastHit hit;
                 Ray ray = new Ray(player.position, Vector3.down);
                 if (Physics.Raycast(ray, out hit, 0.8f))
                 {
                     if (hit.transform.tag == "ToBeAttracted")
                     {
-                        Vector3 newPos = new Vector3(joinObj.position.x, 0, joinObj.position.z);
+                        Vector3 newPos = new Vector3(joinObj.position.x, hit.transform.position.y, joinObj.position.z);
                         hit.transform.position = Vector3.MoveTowards(hit.transform.position, newPos, harpoonForce * Time.deltaTime / 5);
+                        player.position = Vector3.MoveTowards(player.position, newPos, harpoonForce * Time.deltaTime / 5);
+                        return;
                     }
                 }
-            }
-
-            if (Vector3.Distance(player.position, joinObj.position) < platform.destroyDistance)
-            {
-                player.GetComponent<HarpoonSpawn>().sent = false;
-                Destroy(gameObject);
+                player.position = Vector3.MoveTowards(player.position, joinObj.position, harpoonForce * Time.deltaTime);
             }
         }
     }
