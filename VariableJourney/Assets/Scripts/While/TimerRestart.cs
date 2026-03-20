a070085ae7f671070d8354ce1408bb25a82b3a5c
Эмилия <152471759+MilkyWayXDYT@users.noreply.github.com>
2026-03-20 12:10:52 +0300

    Prototipe of third level is done

---
+++ VariableJourney/Assets/Scripts/While/TimerRestart.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimerRestart : MonoBehaviour
+{
+    private float timer = 240f;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+            LevelRestart();
+    }
+
+    private void LevelRestart()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
